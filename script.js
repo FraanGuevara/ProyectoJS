0649294f3ce3fe55f1b81d4009f48034d984b77c
Franco <fraan.guevara1@gmail.com>
2022-02-16 23:51:27 -0300

    add SweetAlert + estilos css

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// SWEETALERT2
+
+// let noValido = [String, null, NaN, "", " "];
+// let noValidoItems = noValido.forEach(item => console.log(item));
+
+
 let n;
 let i = 1;
 const arrayPersonas = [];
@@ -5,14 +11,22 @@ const boton = document.querySelector(".boton");
 
 boton.addEventListener("click", function(){
     arrayPersonas.push(n = prompt("Por favor, ingrese su nombre"))
-    if(n === ""){
+    if(n === null || n === Number || n === "" || n === " "){
     alert("No ingresaste un nombre valido")
-    } else if(i <=5){
-    alert(`Hola ${n} tu mesa es la N° ${i++}`)
+    } else if(i <=3){
+        Swal.fire(
+            `Bienvenid@ ${n} a Cafe San Juan`,
+            'Haz click en el OK para confirmar tu entrada y seleccionar una mesa',
+            'success')
+            i++
     }else{
-        alert("Mil disculpas, no tenemos mesas disponibles")
+        Swal.fire({
+            icon: 'warning',
+            title: 'Oh no!',
+            text: 'Mil disculpas no nos quedan mesas disponibles',
+            footer: '<a href="">Quiero ir pidiendo algo para tomar</a>'})
     };
+    console.log(arrayPersonas)
 }
 )
 
-console.log(arrayPersonas)
